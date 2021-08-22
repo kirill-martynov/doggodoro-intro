17478ed582b3e174929536693a36195f8e77fb97
Kirill Martynov <kirchik.martynov.995@gmail.com>
2021-08-22 19:18:00 +0300

    Remove next/image package, Add next-optimized-images package

---
+++ _static/_next/static/NDxvrJYRM9Ml3hf74ndGI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb291d7de33c874d5a88.css","static/chunks/pages/index-f72de39c5373861c56ac.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
